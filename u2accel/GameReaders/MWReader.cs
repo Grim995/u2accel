160c4c6b90cbe0cefb0e9fb38ebe32a33cea3a1f
Grim Maple <grimmaple95@gmail.com>
2018-09-16 20:03:19 +0300

    Added Most Wanted, some fixing

---
+++ u2accel/GameReaders/MWReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace u2accel
+{
+    class MWReader : GameReader
+    {
+        new public int GameId
+        {
+            get
+            {
+                return 3;
+            }
+        }
+
+        public MWReader() : base()
+        {
+            speedAddress = 0x00914798;
+            name = "speed";
+        }
+    }
+}
